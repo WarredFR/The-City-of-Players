57aeb0aa2cf2e51c6be92756d5776a85d37a1adf
WarredFR <38101418+WarredFR@users.noreply.github.com>
2018-08-14 14:00:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -613,7 +613,7 @@ bot.on("message", async function(message) {
     }
 });
 
-bot.on(message.content.startsWith(prefix + "sondage")){
+if(message.content.startsWith(prefix + "sondage")){
         const sondageSlice = message.content.slice(prefix.length + "sondage".length).trim();
 
         if(message.guild.channels.find('name', 'sondage')){
@@ -630,7 +630,5 @@ bot.on(message.content.startsWith(prefix + "sondage")){
             //Rien ici
         });
     }
-
-
 
 bot.login(process.env.TOKEN);
